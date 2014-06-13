8a86571c9892fb13b41c7bc4ed3d4354abe03ae5
Raymond Lee <raylee247@live.com>
2014-06-13 14:49:31 -0700

    pokemon.sql fix

--- sql/pokemon.sql
+++ sql/pokemon.sql
@@ -1,152 +1,124 @@
 INSERT INTO pokemon (pid, name, picture) VALUES
-(	1	,	Bulbasaur	,	http://img.pokemondb.net/artwork/bulbasaur.jpg	);	,
-(	2	,	Ivysaur	,	http://img.pokemondb.net/artwork/ivysaur.jpg	)	,
-(	3	,	Venusaur	,	http://img.pokemondb.net/artwork/venusaur.jpg	)	,
-(	4	,	Charmander	,	http://img.pokemondb.net/artwork/charmander.jpg	)	,
-(	5	,	Charmeleon	,	http://img.pokemondb.net/artwork/charmeleon.jpg	)	,
-(	6	,	Charizard	,	http://img.pokemondb.net/artwork/charizard.jpg	)	,
-(	7	,	Squirtle	,	http://img.pokemondb.net/artwork/squirtle.jpg	)	,
-(	8	,	Wartortle	,	http://img.pokemondb.net/artwork/wartortle.jpg	)	,
-(	9	,	Blastoise	,	http://img.pokemondb.net/artwork/blastoise.jpg	)	,
-(	10	,	Caterpie	,	http://img.pokemondb.net/artwork/caterpie.jpg	)	,
-(	11	,	Metapod	,	http://img.pokemondb.net/artwork/metapod.jpg	)	,
-(	12	,	Butterfree	,	http://img.pokemondb.net/artwork/butterfree.jpg	)	,
-(	13	,	Weedle	,	http://img.pokemondb.net/artwork/weedle.jpg	)	,
-(	14	,	Kakuna	,	http://img.pokemondb.net/artwork/kakuna.jpg	)	,
-(	15	,	Beedrill	,	http://img.pokemondb.net/artwork/beedrill.jpg	)	,
-(	16	,	Pidgey	,	http://img.pokemondb.net/artwork/pidgey.jpg	)	,
-(	17	,	Pidgeotto	,	http://img.pokemondb.net/artwork/pidgeotto.jpg	)	,
-(	18	,	Pidgeot	,	http://img.pokemondb.net/artwork/pidgeot.jpg	)	,
-(	19	,	Rattata	,	http://img.pokemondb.net/artwork/rattata.jpg	)	,
-(	20	,	Raticate	,	http://img.pokemondb.net/artwork/raticate.jpg	)	,
-(	21	,	Spearow	,	http://img.pokemondb.net/artwork/spearow.jpg	)	,
-(	22	,	Fearow	,	http://img.pokemondb.net/artwork/fearow.jpg	)	,
-(	23	,	Ekans	,	http://img.pokemondb.net/artwork/ekans.jpg	)	,
-(	24	,	Arbok	,	http://img.pokemondb.net/artwork/arbok.jpg	)	,
-(	25	,	Pikachu	,	http://img.pokemondb.net/artwork/pikachu.jpg	)	,
-(	26	,	Raichu	,	http://img.pokemondb.net/artwork/raichu.jpg	)	,
-(	27	,	Sandshrew	,	http://img.pokemondb.net/artwork/sandshrew.jpg	)	,
-(	28	,	Sandslash	,	http://img.pokemondb.net/artwork/sandslash.jpg	)	,
-(	29	,	Nidoran ♀	,	http://img.pokemondb.net/artwork/nidoran-f.jpg	)	,
-(	30	,	Nidorina	,	http://img.pokemondb.net/artwork/nidorina.jpg	)	,
-(	31	,	Nidoqueen	,	http://img.pokemondb.net/artwork/nidoqueen.jpg	)	,
-(	32	,	Nidoran ♂	,	http://img.pokemondb.net/artwork/nidoran-m.jpg	)	,
-(	33	,	Nidorino	,	http://img.pokemondb.net/artwork/nidorino.jpg	)	,
-(	34	,	Nidoking	,	http://img.pokemondb.net/artwork/nidoking.jpg	)	,
-(	35	,	Clefairy	,	http://img.pokemondb.net/artwork/clefairy.jpg	)	,
-(	36	,	Clefable	,	http://img.pokemondb.net/artwork/clefable.jpg	)	,
-(	37	,	Vulpix	,	http://img.pokemondb.net/artwork/vulpix.jpg	)	,
-(	38	,	Ninetales	,	http://img.pokemondb.net/artwork/ninetales.jpg	)	,
-(	39	,	Jigglypuff	,	http://img.pokemondb.net/artwork/jigglypuff.jpg	)	,
-(	40	,	Wigglytuff	,	http://img.pokemondb.net/artwork/wigglytuff.jpg	)	,
-(	41	,	Zubat	,	http://img.pokemondb.net/artwork/zubat.jpg	)	,
-(	42	,	Golbat	,	http://img.pokemondb.net/artwork/golbat.jpg	)	,
-(	43	,	Oddish	,	http://img.pokemondb.net/artwork/oddish.jpg	)	,
-(	44	,	Gloom	,	http://img.pokemondb.net/artwork/gloom.jpg	)	,
-(	45	,	Vileplume	,	http://img.pokemondb.net/artwork/vileplume.jpg	)	,
-(	46	,	Paras	,	http://img.pokemondb.net/artwork/paras.jpg	)	,
-(	47	,	Parasect	,	http://img.pokemondb.net/artwork/parasect.jpg	)	,
-(	48	,	Venonat	,	http://img.pokemondb.net/artwork/venonat.jpg	)	,
-(	49	,	Venomoth	,	http://img.pokemondb.net/artwork/venomoth.jpg	)	,
-(	50	,	Diglett	,	http://img.pokemondb.net/artwork/diglett.jpg	)	,
-(	51	,	Dugtrio	,	http://img.pokemondb.net/artwork/dugtrio.jpg	)	,
-(	52	,	Meowth	,	http://img.pokemondb.net/artwork/meowth.jpg	)	,
-(	53	,	Persian	,	http://img.pokemondb.net/artwork/persian.jpg	)	,
-(	54	,	Psyduck	,	http://img.pokemondb.net/artwork/psyduck.jpg	)	,
-(	55	,	Golduck	,	http://img.pokemondb.net/artwork/golduck.jpg	)	,
-(	56	,	Mankey	,	http://img.pokemondb.net/artwork/mankey.jpg	)	,
-(	57	,	Primeape	,	http://img.pokemondb.net/artwork/primeape.jpg	)	,
-(	58	,	Growlithe	,	http://img.pokemondb.net/artwork/growlithe.jpg	)	,
-(	59	,	Arcanine	,	http://img.pokemondb.net/artwork/arcanine.jpg	)	,
-(	60	,	Poliwag	,	http://img.pokemondb.net/artwork/poliwag.jpg	)	,
-(	61	,	Poliwhirl	,	http://img.pokemondb.net/artwork/poliwhirl.jpg	)	,
-(	62	,	Poliwrath	,	http://img.pokemondb.net/artwork/poliwrath.jpg	)	,
-(	63	,	Abra	,	http://img.pokemondb.net/artwork/abra.jpg	)	,
-(	64	,	Kadabra	,	http://img.pokemondb.net/artwork/kadabra.jpg	)	,
-(	65	,	Alakazam	,	http://img.pokemondb.net/artwork/alakazam.jpg	)	,
-(	66	,	Machop	,	http://img.pokemondb.net/artwork/machop.jpg	)	,
-(	67	,	Machoke	,	http://img.pokemondb.net/artwork/machoke.jpg	)	,
-(	68	,	Machamp	,	http://img.pokemondb.net/artwork/machamp.jpg	)	,
-(	69	,	Bellsprout	,	http://img.pokemondb.net/artwork/bellsprout.jpg	)	,
-(	70	,	Weepinbell	,	http://img.pokemondb.net/artwork/weepinbell.jpg	)	,
-(	71	,	Victreebel	,	http://img.pokemondb.net/artwork/victreebel.jpg	)	,
-(	72	,	Tentacool	,	http://img.pokemondb.net/artwork/tentacool.jpg	)	,
-(	73	,	Tentacruel	,	http://img.pokemondb.net/artwork/tentacruel.jpg	)	,
-(	74	,	Geodude	,	http://img.pokemondb.net/artwork/geodude.jpg	)	,
-(	75	,	Graveler	,	http://img.pokemondb.net/artwork/graveler.jpg	)	,
-(	76	,	Golem	,	http://img.pokemondb.net/artwork/golem.jpg	)	,
-(	77	,	Ponyta	,	http://img.pokemondb.net/artwork/ponyta.jpg	)	,
-(	78	,	Rapidash	,	http://img.pokemondb.net/artwork/rapidash.jpg	)	,
-(	79	,	Slowpoke	,	http://img.pokemondb.net/artwork/slowpoke.jpg	)	,
-(	80	,	Slowbro	,	http://img.pokemondb.net/artwork/slowbro.jpg	)	,
-(	81	,	Magnemite	,	http://img.pokemondb.net/artwork/magnemite.jpg	)	,
-(	82	,	Magneton	,	http://img.pokemondb.net/artwork/magneton.jpg	)	,
-(	83	,	Farfetch'd	,	http://img.pokemondb.net/artwork/farfetch'd.jpg	)	,
-(	84	,	Doduo	,	http://img.pokemondb.net/artwork/doduo.jpg	)	,
-(	85	,	Dodrio	,	http://img.pokemondb.net/artwork/dodrio.jpg	)	,
-(	86	,	Seel	,	http://img.pokemondb.net/artwork/seel.jpg	)	,
-(	87	,	Dewgong	,	http://img.pokemondb.net/artwork/dewgong.jpg	)	,
-(	88	,	Grimer	,	http://img.pokemondb.net/artwork/grimer.jpg	)	,
-(	89	,	Muk	,	http://img.pokemondb.net/artwork/muk.jpg	)	,
-(	90	,	Shellder	,	http://img.pokemondb.net/artwork/shellder.jpg	)	,
-(	91	,	Cloyster	,	http://img.pokemondb.net/artwork/cloyster.jpg	)	,
-(	92	,	Gastly	,	http://img.pokemondb.net/artwork/gastly.jpg	)	,
-(	93	,	Haunter	,	http://img.pokemondb.net/artwork/haunter.jpg	)	,
-(	94	,	Gengar	,	http://img.pokemondb.net/artwork/gengar.jpg	)	,
-(	95	,	Onix	,	http://img.pokemondb.net/artwork/onix.jpg	)	,
-(	96	,	Drowzee	,	http://img.pokemondb.net/artwork/drowzee.jpg	)	,
-(	97	,	Hypno	,	http://img.pokemondb.net/artwork/hypno.jpg	)	,
-(	98	,	Krabby	,	http://img.pokemondb.net/artwork/krabby.jpg	)	,
-(	99	,	Kingler	,	http://img.pokemondb.net/artwork/kingler.jpg	)	,
-(	100	,	Voltorb	,	http://img.pokemondb.net/artwork/voltorb.jpg	)	,
-(	101	,	Electrode	,	http://img.pokemondb.net/artwork/electrode.jpg	)	,
-(	102	,	Exeggcute	,	http://img.pokemondb.net/artwork/exeggcute.jpg	)	,
-(	103	,	Exeggutor	,	http://img.pokemondb.net/artwork/exeggutor.jpg	)	,
-(	104	,	Cubone	,	http://img.pokemondb.net/artwork/cubone.jpg	)	,
-(	105	,	Marowak	,	http://img.pokemondb.net/artwork/marowak.jpg	)	,
-(	106	,	Hitmonlee	,	http://img.pokemondb.net/artwork/hitmonlee.jpg	)	,
-(	107	,	Hitmonchan	,	http://img.pokemondb.net/artwork/hitmonchan.jpg	)	,
-(	108	,	Lickitung	,	http://img.pokemondb.net/artwork/lickitung.jpg	)	,
-(	109	,	Koffing	,	http://img.pokemondb.net/artwork/koffing.jpg	)	,
-(	110	,	Weezing	,	http://img.pokemondb.net/artwork/weezing.jpg	)	,
-(	111	,	Rhyhorn	,	http://img.pokemondb.net/artwork/rhyhorn.jpg	)	,
-(	112	,	Rhydon	,	http://img.pokemondb.net/artwork/rhydon.jpg	)	,
-(	113	,	Chansey	,	http://img.pokemondb.net/artwork/chansey.jpg	)	,
-(	114	,	Tangela	,	http://img.pokemondb.net/artwork/tangela.jpg	)	,
-(	115	,	Kangaskhan	,	http://img.pokemondb.net/artwork/kangaskhan.jpg	)	,
-(	116	,	Horsea	,	http://img.pokemondb.net/artwork/horsea.jpg	)	,
-(	117	,	Seadra	,	http://img.pokemondb.net/artwork/seadra.jpg	)	,
-(	118	,	Goldeen	,	http://img.pokemondb.net/artwork/goldeen.jpg	)	,
-(	119	,	Seaking	,	http://img.pokemondb.net/artwork/seaking.jpg	)	,
-(	120	,	Staryu	,	http://img.pokemondb.net/artwork/staryu.jpg	)	,
-(	121	,	Starmie	,	http://img.pokemondb.net/artwork/starmie.jpg	)	,
-(	122	,	Mr. Mime	,	http://img.pokemondb.net/artwork/mr-mime.jpg	)	,
-(	123	,	Scyther	,	http://img.pokemondb.net/artwork/scyther.jpg	)	,
-(	124	,	Jynx	,	http://img.pokemondb.net/artwork/jynx.jpg	)	,
-(	125	,	Electabuzz	,	http://img.pokemondb.net/artwork/electabuzz.jpg	)	,
-(	126	,	Magmar	,	http://img.pokemondb.net/artwork/magmar.jpg	)	,
-(	127	,	Pinsir	,	http://img.pokemondb.net/artwork/pinsir.jpg	)	,
-(	128	,	Tauros	,	http://img.pokemondb.net/artwork/tauros.jpg	)	,
-(	129	,	Magikarp	,	http://img.pokemondb.net/artwork/magikarp.jpg	)	,
-(	130	,	Gyarados	,	http://img.pokemondb.net/artwork/gyarados.jpg	)	,
-(	131	,	Lapras	,	http://img.pokemondb.net/artwork/lapras.jpg	)	,
-(	132	,	Ditto	,	http://img.pokemondb.net/artwork/ditto.jpg	)	,
-(	133	,	Eevee	,	http://img.pokemondb.net/artwork/eevee.jpg	)	,
-(	134	,	Vaporeon	,	http://img.pokemondb.net/artwork/vaporeon.jpg	)	,
-(	135	,	Jolteon	,	http://img.pokemondb.net/artwork/jolteon.jpg	)	,
-(	136	,	Flareon	,	http://img.pokemondb.net/artwork/flareon.jpg	)	,
-(	137	,	Porygon	,	http://img.pokemondb.net/artwork/porygon.jpg	)	,
-(	138	,	Omanyte	,	http://img.pokemondb.net/artwork/omanyte.jpg	)	,
-(	139	,	Omastar	,	http://img.pokemondb.net/artwork/omastar.jpg	)	,
-(	140	,	Kabuto	,	http://img.pokemondb.net/artwork/kabuto.jpg	)	,
-(	141	,	Kabutops	,	http://img.pokemondb.net/artwork/kabutops.jpg	)	,
-(	142	,	Aerodactyl	,	http://img.pokemondb.net/artwork/aerodactyl.jpg	)	,
-(	143	,	Snorlax	,	http://img.pokemondb.net/artwork/snorlax.jpg	)	,
-(	144	,	Articuno	,	http://img.pokemondb.net/artwork/articuno.jpg	)	,
-(	145	,	Zapdos	,	http://img.pokemondb.net/artwork/zapdos.jpg	)	,
-(	146	,	Moltres	,	http://img.pokemondb.net/artwork/moltres.jpg	)	,
-(	147	,	Dratini	,	http://img.pokemondb.net/artwork/dratini.jpg	)	,
-(	148	,	Dragonair	,	http://img.pokemondb.net/artwork/dragonair.jpg	)	,
-(	149	,	Dragonite	,	http://img.pokemondb.net/artwork/dragonite.jpg	)	,
-(	150	,	Mewtwo	,	http://img.pokemondb.net/artwork/mewtwo.jpg	)	,
-(	151	,	Mew	,	http://img.pokemondb.net/artwork/mew.jpg	)	;
+(	'18',	'Pidgeot'	,	'http://img.pokemondb.net/artwork/pidgeot.jpg'	)	;
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'19',	'Rattata'	,	'http://img.pokemondb.net/artwork/rattata.jpg'	)	;
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'20',	'Raticate'	,	'http://img.pokemondb.net/artwork/raticate.jpg'	)	;
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'21',	'Spearow'	,	'http://img.pokemondb.net/artwork/spearow.jpg'	)	;
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'22',	'Fearow'	,	'http://img.pokemondb.net/artwork/fearow.jpg'	)	;
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'23',	'Ekans'	,	'http://img.pokemondb.net/artwork/ekans.jpg	')	;
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'24',	'Arbok'	,	'http://img.pokemondb.net/artwork/arbok.jpg'	);
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'25',	'Pikachu'	,	'http://img.pokemondb.net/artwork/pikachu.jpg	');
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'26',	'Raichu'	,	'http://img.pokemondb.net/artwork/raichu.jpg	');
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'27',	'Sandshrew'	,	'http://img.pokemondb.net/artwork/sandshrew.jpg	');
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'28',	'Sandslash'	,	'http://img.pokemondb.net/artwork/sandslash.jpg	');
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'29',	'Nidoran'	,	'http://img.pokemondb.net/artwork/nidoran-f.jpg	');
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'30',	'Nidorina'	,	'http://img.pokemondb.net/artwork/nidorina.jpg	');
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'31',	'Nidoqueen'	,	'http://img.pokemondb.net/artwork/nidoqueen.jpg	');
+INSERT INTO pokemon (pid, name, picture) VALUES
+(   '32',	'Nidoran'	,	'http://img.pokemondb.net/artwork/nidoran-m.jpg'	);
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'33',	'Nidorino'	,	'http://img.pokemondb.net/artwork/nidorino.jpg'	)	;
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'34',	'Nidoking'	,	'http://img.pokemondb.net/artwork/nidoking.jpg'	)	;
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'35',	'Clefairy'	,	'http://img.pokemondb.net/artwork/clefairy.jpg'	)	;
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'36',	'Clefable'	,	'http://img.pokemondb.net/artwork/clefable.jpg'	)	;
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'37',	'Vulpix'	,	'http://img.pokemondb.net/artwork/vulpix.jpg'	)	;
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'38',	'Ninetales'	,	'http://img.pokemondb.net/artwork/ninetales.jpg	')	;
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'39',	'Jigglypuff'	,	'http://img.pokemondb.net/artwork/jigglypuff.jpg'	);
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'40',	'Wigglytuff'	,	'http://img.pokemondb.net/artwork/wigglytuff.jpg'	);
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'41',	'Zubat'	,	'http://img.pokemondb.net/artwork/zubat.jpg'	)	;
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'42',	'Golbat'	,	'http://img.pokemondb.net/artwork/golbat.jpg'	);
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'43',	'Oddish'	,	'http://img.pokemondb.net/artwork/oddish.jpg'	);
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'44',	'Gloom'	,	'http://img.pokemondb.net/artwork/gloom.jpg'	)	;
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'45',	'Vileplume'	,	'http://img.pokemondb.net/artwork/vileplume.jpg'	);
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'46',	'Paras'	,	'http://img.pokemondb.net/artwork/paras.jpg'	)	;
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'47',	'Parasect'	,	'http://img.pokemondb.net/artwork/parasect.jpg'	);
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'48',	'Venonat',	'http://img.pokemondb.net/artwork/venonat.jpg'	)	;
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'49',	'Venomoth'	,	'http://img.pokemondb.net/artwork/venomoth.jpg'	);
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'50',	'Diglett'	,	'http://img.pokemondb.net/artwork/diglett.jpg'	);
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'51',	'Dugtrio'	,	'http://img.pokemondb.net/artwork/dugtrio.jpg'	);
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'52',	'Meowth'	,	'http://img.pokemondb.net/artwork/meowth.jpg'	);
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'53',	'Persian'	,	'http://img.pokemondb.net/artwork/persian.jpg'	);
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'54',	'Psyduck'	,	'http://img.pokemondb.net/artwork/psyduck.jpg'	);
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'55',	'Golduck'	,	'http://img.pokemondb.net/artwork/golduck.jpg'	);
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'56',	'Mankey'	,	'http://img.pokemondb.net/artwork/mankey.jpg'	);
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'57',	'Primeape'	,	'http://img.pokemondb.net/artwork/primeape.jpg'	);
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'58',	'Growlithe'	,	'http://img.pokemondb.net/artwork/growlithe.jpg');	
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'59',	'Arcanine'	,	'http://img.pokemondb.net/artwork/arcanine.jpg'	);	
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'60',	'Poliwag'	,	'http://img.pokemondb.net/artwork/poliwag.jpg'	);	
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'61',	'Poliwhirl'	,	'http://img.pokemondb.net/artwork/poliwhirl.jpg'	);	
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'62',	'Poliwrath'	,	'http://img.pokemondb.net/artwork/poliwrath.jpg'	);	
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'63',	'Abra'	,	'http://img.pokemondb.net/artwork/abra.jpg'	)	;
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'64',	'Kadabra'	,	'http://img.pokemondb.net/artwork/kadabra.jpg'	);	
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'65',	'Alakazam'	,	'http://img.pokemondb.net/artwork/alakazam.jpg'	);	
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'66',	'Machop'	,	'http://img.pokemondb.net/artwork/machop.jpg'	);	
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'67',	'Machoke'	,	'http://img.pokemondb.net/artwork/machoke.jpg'	);	
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'68',	'Machamp'	,	'http://img.pokemondb.net/artwork/machamp.jpg'	);	
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'69',	'Bellsprout'	,	'http://img.pokemondb.net/artwork/bellsprout.jpg'	);	
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'70',	'Weepinbell'	,	'http://img.pokemondb.net/artwork/weepinbell.jpg'	);	
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'71',	'Victreebel'	,	'http://img.pokemondb.net/artwork/victreebel.jpg'	);	
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'72',	'Tentacool'	,	'http://img.pokemondb.net/artwork/tentacool.jpg'	)	;
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'73',	'Tentacruel'	,	'http://img.pokemondb.net/artwork/tentacruel.jpg'	);	
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'74',	'Geodude'	,	'http://img.pokemondb.net/artwork/geodude.jpg'	)	;
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'75',	'Graveler'	,	'http://img.pokemondb.net/artwork/graveler.jpg'	)	;
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'76',	'Golem'	,	'http://img.pokemondb.net/artwork/golem.jpg'	)	;
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'88',	'Grimer'	,	'http://img.pokemondb.net/artwork/grimer.jpg'	)	;
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'91',	'Cloyster'	,	'http://img.pokemondb.net/artwork/cloyster.jpg'	)	;
+INSERT INTO pokemon (pid, name, picture) VALUES
+(	'93',	'Haunter'	,	'http://img.pokemondb.net/artwork/haunter.jpg'	)	;
