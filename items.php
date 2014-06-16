5e1c9e8cffc01f6d9d74df36f93c77c8d574a20e
afung22 <alison_fung@dhcp-128-189-85-36.ubcsecure.wireless.ubc.ca>
2014-06-16 16:44:29 -0700

    New Search Bar

--- items.php
+++ items.php
@@ -1,16 +1,15 @@
-<!DOCTYPE HTML>
-<!--
-	Aerial 1.0 by HTML5 UP
-	html5up.net | @n33co
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
--->
 <html>
 	<?php
 	require 'template.php';
-	echo $header;
+	session_start();
+	ini_set('session.save_path', '/home/i/i5a8/');
 	?>
-
-				<!-- Header -->
+                <head>
+                <title>POKEGUIDE</title>
+                </head>
+                <body bgcolor="#40E0D0">
+                
+                <!-- Header -->
 					<header id="menu">
                 	<nav>
                		 <ul>
@@ -29,57 +28,39 @@
 						</nav>
 					</header>
 
-<form name="input" action="items.php" method="get">
-Item: <input type="text" name="item">
+
+<form name="search" action="itemtest.php" method="post">
+Search Items: <input type="text" name="item"> with
+<select id = "options" name = "options">
+<option value= "IID"> IID</option>
+<option value= "Type"> Type </option>
+<option value= "Description">  Description </option>
+</select>
 <input type="submit" value="search">
 </form> 
- 
-                    <body>
-                		<table>
-                        <thead>
-                    	<tr>
-                    		<td>IID</td>
-                    		<td>Type</td>
-                    	</tr>
-                        </thead>
-                        <tbody>
-							<?php
-							$result = executePlainSQL("select * from item");
-							echo "<table>";
-							while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
-								echo "<tr><td>" . $row['iid']. "</td><td>" . $row['type'] . "</td></tr>";  	
-								}
-								echo "</table>";
-							}
-							?>
-                   		 </tbody>
-                     	 </table>
-                     </body>
-                
-             
-<?php
 
-//SEARCH
-	$item = ucfirst($_GET["item"]);
-	if($item != null){
-		$result = executePlainSQL("select * from item where type = '" . $item . "'");
-	}
-	else{
-		$result = executePlainSQL("select * from item");
-	}
-	printItem($result);
+
+<?php
+		if(isset($_POST['item'])){
+                 $item = $_POST ['options'];
+//              echo $item;
+         }
+         $item = ucfirst(strtolower($_POST["item"]));
+		 if($item !==null){
+			 $result = executePlainSQL("SELECT * FROM item WHERE type = '" . $item . "' OR iid = '" . $item ."' OR Description LIKE '%" . $item . "%'");
+		}else{
+			$result = executePlainSQL("SELECT * FROM item");
+		}
+		printItem($result);
 	
-//Print result	
-function printPoke($result){
-	echo '<table>';
-	echo '<tr><td>IID</td><td>Type</td></tr>';
+function printItem($result){
+	echo "<table border = 1>";
+	echo "<tr><th>IID</th><th>Type</th><th>Description</th></tr>";
 	while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
-		//echo "<tr><td>" . $row[0] . "</td><td><a href = profile.php?name=" . $row[1] . ">" . $row[1] . "</a></td><td>" . '<img src="' . $row[2] . '" alt="picture">' . "</td></tr>";
-		//echo '<tr><img src="' . $row[2] . '" alt="picture"></tr>';								
-	}//<a href="pokemon.php" class="classname">Pokemon</a>
-	echo '</table>';
+		echo "<tr><td>" . $row[0] . "</td><td>" . $row[1] . "</td><td>" . $row[2] . "</td></tr>";																	
+	}	
+	echo "</table>";
 }
-?>
-                    
-	<!-->OCI_close();<-->
+?>       
+                        
 </html>
