7f7cbec1d96336c3dbedefa626ee6c82ca6bf03f
Martin Tsang <m4rtin.t@gmail.com>
2014-06-16 16:37:16 -0700

    folder name changes
    css -> cssmain
    js ->jsmain

--- index.php
+++ index.php
@@ -10,19 +10,18 @@
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 		<meta name="description" content="" />
 		<meta name="keywords" content="" />
-		<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
-		<script src="js/skel.min.js"></script>
-		<script src="js/init.js"></script>
+		<!--[if lte IE 8]><script src="cssmain/ie/html5shiv.js"></script><![endif]-->
+		<script src="jsmain/skel.min.js"></script>
+		<script src="jsmain/init.js"></script>
 		<noscript>
-			<link rel="stylesheet" href="css/skel.css" />
-			<link rel="stylesheet" href="css/style.css" />
-			<link rel="stylesheet" href="css/style-wide.css" />
-			<link rel="stylesheet" href="css/style-noscript.css" />
+			<link rel="stylesheet" href="cssmain/skel.css" />
+			<link rel="stylesheet" href="cssmain/style.css" />
+			<link rel="stylesheet" href="cssmain/style-wide.css" />
+			<link rel="stylesheet" href="cssmain/style-noscript.css" />
 		</noscript>
-		<!--[if lte IE 9]><link rel="stylesheet" href="css/ie/v9.css" /><![endif]-->
-		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="cssmain/ie/v9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="cssmain/ie/v8.css" /><![endif]-->
 	</head>
-	
 	<body class="loading">
 		<div id="wrapper">
 			<div id="bg"></div>
@@ -38,10 +37,10 @@
 							<li><a href="pokemon.php" class="classname">Pokemon</a></li>
 							  <li><a href="location.php" class="classname">Location</a></li>
 							  <li><a href="moves.php" class="classname">Moves</a></li>
-								<li><a href="items.php" class="classname">Items</a></li>
+							 <li> <a href="items.php" class="classname">Items</a></li>
 								<li><a href="trainers.php" class="classname">Trainers</a></li>
-                               <li><a href="types.php" class="classname">Types</a></li>
-                               <li><a href="teamBuilder.php" class="classname"> Team Builder</a>
+                               <li><a href="types.php" class="classname">Types</a>
+                               <li><a href="teamBuilder.php" class="classname"> Team Buidler </a></li>
 							</ul>
 						</nav>
 					</header>
