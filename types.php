864c8b74e72f3e7b31e4c7e84e7428ed38776c82
Martin Tsang <m4rtin.t@gmail.com>
2014-06-17 20:01:13 -0700

    UI fixes

--- types.php
+++ types.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 	
-		<title>PokeGude</title>
+		<title>PokeGuide</title>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 		<meta name="description" content="" />
 		<meta name="keywords" content="" />
