05d647500ad6749cb0626266ee662b6c595f8304
Martin Tsang <m4rtin.t@gmail.com>
2014-06-17 17:28:21 -0700

    trainer UI update

--- trainers.php
+++ trainers.php
@@ -1,166 +1,56 @@
 <!DOCTYPE HTML>
-<!--
-	Aerial 1.0 by HTML5 UP
-	html5up.net | @n33co
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
--->
 <html>
-	<?php
-	echo $header;
-	require 'template.php';
-	session_start();
-	?>
-
-<head>
-    <style>
-    .Pokeguide {
-	margin:0px;padding:0px;
-	width:100%;
-	box-shadow: 10px 10px 5px #888888;
-	border:1px solid #ffffff;
-	
-	-moz-border-radius-bottomleft:3px;
-	-webkit-border-bottom-left-radius:3px;
-	border-bottom-left-radius:3px;
-	
-	-moz-border-radius-bottomright:3px;
-	-webkit-border-bottom-right-radius:3px;
-	border-bottom-right-radius:3px;
-	
-	-moz-border-radius-topright:3px;
-	-webkit-border-top-right-radius:3px;
-	border-top-right-radius:3px;
-	
-	-moz-border-radius-topleft:3px;
-	-webkit-border-top-left-radius:3px;
-	border-top-left-radius:3px;
-}.Pokeguide table{
-    border-collapse: collapse;
-        border-spacing: 0;
-	width:100%;
-	height:100%;
-	margin:0px;padding:0px;
-}.Pokeguide tr:last-child td:last-child {
-	-moz-border-radius-bottomright:3px;
-	-webkit-border-bottom-right-radius:3px;
-	border-bottom-right-radius:3px;
-}
-.Pokeguide table tr:first-child td:first-child {
-	-moz-border-radius-topleft:3px;
-	-webkit-border-top-left-radius:3px;
-	border-top-left-radius:3px;
-}
-.Pokeguide table tr:first-child td:last-child {
-	-moz-border-radius-topright:3px;
-	-webkit-border-top-right-radius:3px;
-	border-top-right-radius:3px;
-}.Pokeguide tr:last-child td:first-child{
-	-moz-border-radius-bottomleft:3px;
-	-webkit-border-bottom-left-radius:3px;
-	border-bottom-left-radius:3px;
-}.Pokeguide tr:hover td{
-	background-color:#ffffff;
-		
-
-}
-.Pokeguide td{
-	vertical-align:middle;
-	
-	background-color:#8fc1ea;
-
-	border:1px solid #ffffff;
-	border-width:0px 1px 1px 0px;
-	text-align:center;
-	padding:13px;
-	font-size:14px;
-	font-family:Verdana;
-	font-weight:normal;
-	color:#6d6d6d;
-}.Pokeguide tr:last-child td{
-	border-width:0px 1px 0px 0px;
-}.Pokeguide tr td:last-child{
-	border-width:0px 0px 1px 0px;
-}.Pokeguide tr:last-child td:last-child{
-	border-width:0px 0px 0px 0px;
-}
-.Pokeguide tr:first-child td{
-		background:-o-linear-gradient(bottom, #3371af 5%, #3371af 100%);	background:-webkit-gradient( linear, left top, left bottom, color-stop(0.05, #3371af), color-stop(1, #3371af) );
-	background:-moz-linear-gradient( center top, #3371af 5%, #3371af 100% );
-	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr="#3371af", endColorstr="#3371af");	background: -o-linear-gradient(top,#3371af,3371af);
-
-	background-color:#3371af;
-	border:0px solid #ffffff;
-	text-align:center;
-	border-width:0px 0px 1px 1px;
-	font-size:15px;
-	font-family:Helvetica;
-	font-weight:bold;
-	color:#ffffff;
-}
-.Pokeguide tr:first-child:hover td{
-	background:-o-linear-gradient(bottom, #3371af 5%, #3371af 100%);	background:-webkit-gradient( linear, left top, left bottom, color-stop(0.05, #3371af), color-stop(1, #3371af) );
-	background:-moz-linear-gradient( center top, #3371af 5%, #3371af 100% );
-	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr="#3371af", endColorstr="#3371af");	background: -o-linear-gradient(top,#3371af,3371af);
-
-	background-color:#3371af;
-}
-.Pokeguide tr:first-child td:first-child{
-	border-width:0px 0px 1px 0px;
-}
-.Pokeguide tr:first-child td:last-child{
-	border-width:0px 0px 1px 1px;
-}
-    </style>
-    </head>
-
-				<!-- Header -->
-					 <header id="menu">
-                	<nav>
-               		 <ul>
-                		<li><a href="index.php" class="classname">Back To Home</a></li>
-                	</ul>
-                    </nav>
-						<nav>
-							<ul>
-                            <li><a href="pokemon.php" class="classname">Pokemon</a></li>
-							  <li><a href="location.php" class="classname">Location</a></li>
-							  <li><a href="moves.php" class="classname">Moves</a></li>
-                                <li> <a href="items.php" class="classname">Items</a></li>
-                               <li><a href="types.php" class="classname">Types</a>
-                               <li><a href="teamBuilder.php" class="classname"> Team Buidler </a></li>
-							</ul>
-						</nav>
-					</header>
-
-				<body>
-                		<table>
-                        <thead>
-                    	<tr>
-                    		<td>tid</td>
-                    		<td>party</td>
-                            <td>password</td>
-                    	</tr>
-                        </thead>
-                        <tbody>
-							<?php
-							$result = executePlainSQL("select * from trainer");
-							echo "<table>";
-							while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
-								echo "<tr><td>" . $row['tid']. "</td><td>" . $row['party'] . "</td><td>" . $row['password'] . "</td></tr>";  	
-								echo "</table>";
-							?>
-                   		 </tbody>
-                     	 </table>
-                	 </body>
-
-<form name="input" action="trainers.php" method="get">
-Trainer Name: <input type="text" name="trainer">
-<input type="submit" value="search">
-</form> 
- 
-<?php
+	<head>
+		<title>PokeGude</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<link href='http://fonts.googleapis.com/css?family=Ubuntu+Condensed' rel='stylesheet' type='text/css'>
+		<script src="js/jquery.min.js"></script>
+		<script src="js/config.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-panels.min.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel-noscript.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-desktop.css" />
+		</noscript>
+		<link rel="stylesheet" href="cssmain/table.css" />
+		<!--[if lte IE 9]><link rel="stylesheet" href="css/ie9.css" /><![endif]-->
+		<!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
+	</head>
+	<body>
+	<!-- ********************************************************* -->
+		<div id="header-wrapper">
+			<div class="container">
+				<div class="row">
+					<div class="12u">
+						
+						<?php require 'topMenubar.php';?>
+					
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="main">
+			<div class="container">
+				<div class="row main-row">
+					<div class="12u">
+						
+						<h2 style='text-align:center;'>List Of Trainers</h2>
+						
+						<form name="input" action="trainers.php" method="get">
+						Trainer Name: <input type="text" name="trainer">
+						<input type="submit" value="search">
+						</form> 
+						<br>
+						<?php
+						
 	$trainer = ucfirst(strtolower($_GET["trainer"]));
-	if($trainer != null){
+	if($trainer != null)
+	{
+		$trainers = array_map('ucfirst',explode(" ", $trainer));
+		$trainer = implode(" ", $trainers);
 		echo "<br>Search for " . $_GET["trainer"] . "<br>";  
 		//Finds trainers 	
 		$search_result = executePlainSQL("SELECT COUNT(*) FROM trainer WHERE tid = '" . $trainer ."' OR tid LIKE '%" . $trainer . "%'");
@@ -185,49 +75,102 @@ Trainer Name: <input type="text" name="trainer">
 		//Display information of each trainer that contains %trainer%
 		if($count[0] == 1){
 			$party = explode("," , $name[1]);
-			echo "<br>Name:" . $name[0] . "<br>";
-			echo "<br>Location:" . $location[0] . "<br>";
-			echo "Party:";
+			echo "<div class='Pokeguide'>";
+			echo "<table>";
+			echo "<tr>";
+			echo "<td>Name<br><br><u>{$name[0]}</u></td>";
+			echo "<td>Location<br><br><u>{$location[0]}</u></td>";
 			for($i = 0; $i < count($party); $i++){
-				echo "<tr><td>";
+				echo "<tr>";
+				echo "<td colspan = '2'>";
 				$result = executePlainSQL("SELECT * FROM pokemon WHERE pid = '" . $party[$i] . "'");
-				printTrainer($result);
-				echo "</td></tr>";
+				printPoke($result);
+				echo "</td>";
+				echo "</tr>";
 			}
+			echo "</table>";
+			echo "</div>";
 		}			
 		if($count[0] > 1){
 			while($row = OCI_Fetch_Array($trainer_info, OCI_BOTH)){
-				echo "<br>Name:" . $row[0] . "<br>";
-				echo "<br>Location:" . $location[0] . "<br>";
-				echo "Party:";
+				echo "<div class='Pokeguide'>";
+				echo "<table>";
+				echo "<tr>";
+				echo "<td>Name<br><br><u>{$row[0]}</u></td>";
+				echo "<td>Location<br><br><u>{$location[0]}</u></td>";
 				$party = explode(',' , $row[1]);
 				for($i = 0; $i < count($party); $i++){
-					echo "<tr><td>";
+					echo "<tr>";
+					echo "<td colspan='2'>";
 					$result = executePlainSQL("SELECT * FROM pokemon WHERE pid = '" . $party[$i] . "'");
-					printTraier($result);
-					echo "</td></tr>";
+					printPoke($result);
+					echo "</td>";
+					echo "</tr>";
 					}
+				echo "</table>";
+				echo "</div>";
 			}
 		}
 		
 	}
 	else{
 		$regular = executePlainSQL("SELECT tid FROM trainer ORDER BY tid");
-		printResult($regular);
+		printTable($regular);
 	}
 	
-function printTrainer($result){
-	echo "<div class='Pokeguide'>";
-	echo '<table>';
-	echo '<tr><td>Name</td><td>Image</td></tr>';
+function printPoke($result)
+{
+	while ($row = OCI_Fetch_Array($result, OCI_BOTH)) 
+	{
+		echo "<font size='15px'><a href = profile.php?name='{$row[1]}'>{$row[1]}</a></font><br><br><img src='{$row[2]}' alt='picture'>";
+									
+	}
+}
+
+function printTable($result){
+echo "<div class='Pokeguide'>";
+		echo "<table>";
+		echo "<tr>";
+		echo "<td>Trainer Name</td>";
+		echo "</tr>";
+		
 	while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
-		echo "<tr><td><a href = profile.php?name=" . $row[1] . ">" . $row[1] . "</a></td><td>" . '<img src="' . $row[2] . '" alt="picture">' . "</td></tr>";
-		//echo '<tr><img src="' . $row[2] . '" alt="picture"></tr>';								
-	}//<a href="pokemon.php" class="classname">Pokemon</a>
-	echo '</table>';
-	echo '</div>';
+					for ($i = 0; $i < 15; $i++){
+					if($row[$i] != null)
+						echo "<tr><td>" . $row[$i] . "</td></tr>"; //or just use "echo $row[0]" 
+					}
+					
+				}
+		echo "</table>";
+		echo "</div>";
 }
 ?>
-                    
-	<!-->OCI_close();<-->
+						
+					
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="footer-wrapper">
+			<div class="container">
+				<div class="row">
+					<div class="8u">
+					</div>
+					<div class="4u">
+
+					</div>
+				</div>
+				<div class="row">
+					<div class="12u">
+
+						<div id="copyright">
+									&copy; CS304 - PokeGuide. All rights reserved. | Design: HTML5 UP | Images: fotogrph
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	<!-- ********************************************************* -->
+	</body>
 </html>
